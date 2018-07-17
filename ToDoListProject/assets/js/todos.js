c64f15778cb9fba21aa54c07a2115446cc8111a6
suhyung kwon <ospac111@gmail.com>
2018-07-17 23:46:38 +0900

    styling toDo

--- ToDoListProject/assets/js/todos.js
+++ ToDoListProject/assets/js/todos.js
@@ -1,13 +1,21 @@
 // Check off Specific Todos By clicking
-$("li").on("click",function(){
+$("ul").on("click","li",function(){
     $(this).toggleClass("checked");
 });
 
 //Click on X to delete a Todo
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
     //parent() is for deleting parent element(li) of 'this' span
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     })
     event.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li>" + "<span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>");
+    }
+})
